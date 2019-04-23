7db43270ff6954416d65b65f19da14ba30f69fbd
jaeanderson <JRA@Macintosh-002332b2e698.local>
2019-04-23 03:19:07 -0600

    unlucky13 sum

--- unlucky13.py
+++ unlucky13.py
@@ -10,47 +10,49 @@ import argparse
 import os
 import sys
 
-#---------------------------------------------------------
+# ---------------------------------------------------------
 def get_args():
     """get command-line arguments"""
     parser = argparse.ArgumentParser(
-    description='Unlucky 13 Sum',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Unlucky 13 Sum",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument(
-        'file',
-        help='Input File',
-        metavar='FILE')
+    parser.add_argument("file", help="Input File", metavar="FILE")
 
     return parser.parse_args()
 
-#---------------------------------------------------------
+
+# ---------------------------------------------------------
 def warn(msg):
     """Print a message to STDERR"""
     print(msg, file=sys.stderr)
 
-#---------------------------------------------------------
-def die(msg='Something bad happened'):
+
+# ---------------------------------------------------------
+def die(msg="Something bad happened"):
     """warn() and exit with error"""
     warn(msg)
     sys.exit(1)
 
-#---------------------------------------------------------
+
+# ---------------------------------------------------------
 def unlucky13(nums):
     """sum numbers to 13"""
 
     sum_list = []
     for i in range(0, len(nums)):
-        if (nums[i] == '13'):
+        if nums[i] == "13":
             continue
-        elif (nums[i-1] == '13') and i != 0:
+        elif (nums[i - 1] == "13") and i != 0:
             continue
         else:
             sum_list.append(int(nums[i]))
- 
+
     return sum(sum_list)
 
-#---------------------------------------------------------
+
+# ---------------------------------------------------------
 def main():
     """Sko-Codin!"""
 
@@ -63,13 +65,12 @@ def main():
     luckysum = 0
     input_fh = open(inputfile).read().split()
     for i, line in enumerate(input_fh):
-        numlist = line.split(',')
-        #print(numlist)
+        numlist = line.split(",")
+        # print(numlist)
         luckysum = unlucky13(nums=numlist)
-        print('lucky sum: {}'.format(luckysum))
+        print("lucky sum: {}".format(luckysum))
 
-#----------------------------------------------------------
-if __name__ == '__main__':
+
+# ----------------------------------------------------------
+if __name__ == "__main__":
     main()
-
-

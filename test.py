394fa4345a70dc3367e9aadb6d96a528fc80dc7f
jaeanderson <JRA@Macintosh-002332b2e698.local>
2019-04-23 03:20:18 -0600

    unlucky13 sum

--- test.py
+++ test.py
@@ -3,35 +3,41 @@
 
 from subprocess import getstatusoutput
 
-prg = './unlucky13.py'
-luckynums1 = './luckynums1.txt'
-luckynums2 = './luckynums2.txt'
-luckynums3 = './luckynums3.txt'
+prg = "./unlucky13.py"
+luckynums1 = "./luckynums1.txt"
+luckynums2 = "./luckynums2.txt"
+luckynums3 = "./luckynums3.txt"
 
-#----------------------------------------------------------
+# ----------------------------------------------------------
 def test_usage():
-    rv, out = getstatusoutput('{}'.format(prg))
+    rv, out = getstatusoutput("{}".format(prg))
     assert rv != 0
-    assert out.lower().startswith('usage')
+    assert out.lower().startswith("usage")
 
-#----------------------------------------------------------
+
+# ----------------------------------------------------------
 def test_bad_input():
     """bad input"""
-    (retval, out) = getstatusoutput('{} {}'.format(prg, 'foo'))
+    (retval, out) = getstatusoutput("{} {}".format(prg, "foo"))
     assert retval > 0
     assert out == '"foo" is not a file'
 
-#----------------------------------------------------------
+
+# ----------------------------------------------------------
 def test_runs_ok():
-    rv1, out1 = getstatusoutput('{} {}'.format(prg, luckynums1))
+    rv1, out1 = getstatusoutput("{} {}".format(prg, luckynums1))
     assert rv1 == 0
-    assert out1 == 'lucky sum: 6\nlucky sum: 3\nlucky sum: 2\nlucky sum: 1\nlucky sum: 6'.format(prg)
+    assert (
+        out1
+        == "lucky sum: 6\nlucky sum: 3\nlucky sum: 2\nlucky sum: 1\nlucky sum: 6".format(
+            prg
+        )
+    )
 
-    rv2, out2 = getstatusoutput('{} {}'.format(prg, luckynums2))
+    rv2, out2 = getstatusoutput("{} {}".format(prg, luckynums2))
     assert rv2 == 0
-    assert out2 == 'lucky sum: 0\nlucky sum: 0\nlucky sum: 0'.format(prg)
+    assert out2 == "lucky sum: 0\nlucky sum: 0\nlucky sum: 0".format(prg)
 
-    rv3, out3 = getstatusoutput('{} {}'.format(prg, luckynums3))
+    rv3, out3 = getstatusoutput("{} {}".format(prg, luckynums3))
     assert rv3 == 0
-    assert out3 == 'lucky sum: 3\nlucky sum: 6\nlucky sum: 4'.format(prg)
-
+    assert out3 == "lucky sum: 3\nlucky sum: 6\nlucky sum: 4".format(prg)
